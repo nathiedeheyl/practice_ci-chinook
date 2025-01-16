6973234bdbb0af0d76541d258fe3e12972f5200f
Nathalie von <nath.vonheyl@gmail.com>
2025-01-16 15:59:55 +0000

    feat: set up py script to connect to chinook db and fetch results

---
+++ sql-psycopg2.py
@@ -0,0 +1,19 @@
+import psycopg2
+
+
+# connect to "chinook" database
+connection = psycopg2.connect(database="chinook")
+
+# build a cursor object of the database
+cursor = connection.cursor()
+
+# fetch the results (multiple)
+results = cursor.fetchall()
+
+# fetch the result (single)
+# results = cursor.fetchone()
+
+# close the connection 
+connection.close()
+for results in results:
+    print(result)
